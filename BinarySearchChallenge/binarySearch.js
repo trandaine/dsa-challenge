fc5093664d0d773e20a11f4c944d8422182ec6c7
trandaine <tranquangdai03072004@outlook.com.vn>
2026-07-15 17:14:22 +0700

    complete merge binary search challenge

---
+++ BinarySearchChallenge/binarySearch.js
@@ -0,0 +1,48 @@
+/*
+Algorithm: Binary Search
+Input: array of sorted numbers, and target
+Output: index of target in array, or -1 if not found
+
+Steps:
+1. Init l to 0.
+2. Init r to point to the last element in the array.
+3. Loop as long as l did not pass r:
+4.   Init m to be the middle point between l and r.
+5.   If the value at m is the target:
+6.     Return m.
+7.   Else if the value at m is less than the target:
+8.     Move l to right after m.
+9.   Else:
+10.    Move r to right before m.
+11. Return -1.
+*/
+
+/*
+Challenge:
+***********
+
+Implement the binary search algorithm. Call the function binarySearch() and make sure to export it. When done, run the tests in the terminal with ’npm test’ to make sure it works.
+
+Note: If needed, see hint.md for the formula.
+*/
+
+export function binarySearch(arr, target) {
+  let l = 0;
+  let r = arr.length - 1;
+
+  while (l <= r) {
+    const m = Math.floor((l + r) / 2);
+
+    if (arr[m] === target) {
+      return m;
+    }
+
+    if (arr[m] < target) {
+      l = m + 1;
+    } else {
+      r = m - 1;
+    }
+  }
+
+  return -1;
+}
